169905f0ec9645748483d48cb59b1d79108de589
Hiroshi Yoshida <hrysd22@gmail.com>
2013-05-07 20:25:16 +0900

    Add AA

--- lib/nippo/cli.rb
+++ lib/nippo/cli.rb
@@ -5,6 +5,7 @@ module Nippo
   class Cli < Thor
     desc 'init [TITLE]', 'Initialize task.'
     def init(title)
+      welcome
       Nippo::Generator.setup(title, current_dir)
     end
 
@@ -27,6 +28,27 @@ module Nippo
     private
       def editor
         ENV['EDITOR'] or raise 'Could not find $EDITOR'
+      end
+
+      def welcome
+        puts <<-TEXT
+
+    Let's
+
+
+    b.             8  8 8888 8 888888888o   8 888888888o       ,o888888o.
+    888o.          8  8 8888 8 8888    `88. 8 8888    `88.  . 8888     `88.
+    Y88888o.       8  8 8888 8 8888     `88 8 8888     `88 ,8 8888       `8b
+    .`Y888888o.    8  8 8888 8 8888     ,88 8 8888     ,88 88 8888        `8b
+    8o. `Y888888o. 8  8 8888 8 8888.   ,88' 8 8888.   ,88' 88 8888         88
+    8`Y8o. `Y88888o8  8 8888 8 888888888P'  8 888888888P'  88 8888         88
+    8   `Y8o. `Y8888  8 8888 8 8888         8 8888         88 8888        ,8P
+    8      `Y8o. `Y8  8 8888 8 8888         8 8888         `8 8888       ,8P
+    8         `Y8o.`  8 8888 8 8888         8 8888          ` 8888     ,88'
+    8            `Yo  8 8888 8 8888         8 8888             `8888888P'
+
+
+        TEXT
       end
 
       def current_dir
